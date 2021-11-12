5b1704f994e3356946919b3f88ab9f622c1c62ef
Manel <manelkld369@gmail.com>
2021-11-12 16:25:25 +0100

    Fix home ctrller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,28 +13,8 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-<<<<<<< HEAD:src/Controller/MainController.php
-        return $this->render('home/index.html.twig');
-    }
-
-
-    /**
-     * @Route("/Inscription/{prenom?Hamid}", name="Inscription",
-     *  methods={"GET","POST"}, host="localhost",
-     * schemes={"http", "https"})
-     */
-    public function Inscription(Request $request)
-    {
-
-        $prenom = $request->get('prenom');
-
-        return $this->render(
-            'home/inscription.html.twig'
-        );
-=======
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
->>>>>>> develop:src/Controller/HomeController.php
     }
 }
