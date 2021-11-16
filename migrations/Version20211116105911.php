7a8f0a6331098f637bdbc5db017569f81f6c6cac
Hamidouu22 <hamid.dumiot@hotmail.fr>
2021-11-16 15:01:11 +0100

    EasyAdmin,+paginator+ vichuploader

--- migrations/Version20211116105911.php
+++ migrations/Version20211116105911.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211026135410 extends AbstractMigration
+final class Version20211116105911 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20211026135410 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE publication_id publication_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE publication ADD updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE publication_id publication_id INT NOT NULL');
+        $this->addSql('ALTER TABLE publication DROP updated_at');
     }
 }
