f516b09421655493e839ec8a86eed239a7e49878
hugo <hcrochet14@gmail.com>
2021-11-16 11:22:41 +0100

    changement mot de passe

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -5,17 +5,33 @@ namespace App\Controller;
 use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountPasswordController extends AbstractController
 {
     /**
      * @Route("/compte/modificationMotDePasse", name="account_password")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
+        
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('old_password')->getData();
+            if ($encoder->isPasswordValid($user, $old_pwd)){
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->encodePassword($user, $new_pwd);
+                $user->setPassword($password);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }
+        }
         return $this->render('account/password.html.twig', [
             'form' => $form->createView()
         ]);
