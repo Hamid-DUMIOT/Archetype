d54e9362c1e1ccae6b55dd4556abf890f944c06e
Hamidouu22 <hamid.dumiot@hotmail.fr>
2021-12-04 23:29:44 +0100

    espace commentaire

--- src/Form/CommentaireFormType.php
+++ src/Form/CommentaireFormType.php
@@ -6,13 +6,18 @@ use App\Entity\Commentaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CommentaireFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contenuCom');
+        ->add('ContenuCom', TextType::class, [
+            'label' => 'Envoyer un commentaire ci-dessous',
+           
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
