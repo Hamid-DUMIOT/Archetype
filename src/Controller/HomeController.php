b59fffa6ec8c600fa7deb90c733866c52e1b4ed8
Hamidouu22 <hamid.dumiot@hotmail.fr>
2021-12-04 17:32:11 +0100

    carousel projet dynamique et redimensionné + fix footer contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Projet;
+use App\Repository\ProjetRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+
+
+ 
 class HomeController extends AbstractController
 {
+
+    private $entityManager;
+
+    
     /**
      * @Route("/", name="home")
+     * 
+     *
      */
-    public function index(): Response
+
+  
+
+
+    public function index(ProjetRepository $projetRepository): Response
+    
     {
+        $data =$projetRepository->findAll();
+
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'projets' => $data ,
+            
         ]);
     }
 }
